daeb168067acb1652fb9f6c66417aca4b038cbb2
aliceh1 <149112426+ahobbs-716@users.noreply.github.com>
2024-08-08 16:27:50 -0400

    Updates to bidding mechanism

--- src/words/g0/Group3Player.java
+++ src/words/g0/Group3Player.java
@@ -6,29 +6,53 @@ import java.io.*;
 import java.util.*;
 import java.util.logging.Level;
 
+import static words.core.ScrabbleValues.*;
+
 
 public class Group3Player extends Player {
 
-    //to enable tracking
-    Map<Integer, Map<Letter, Integer>> playerCounts;             //format: Map <PlayerID <letter_type, num_letters_of_type>
-    Map<Letter, Integer> absoluteCounts;                         //format: Map <Letter, num_instances_on_board>
-    int totalPlayed;                                             //total letters that have been purchased by any player in the game. Does not include secret letters/
-    String[] sortWords;                                         //sorted list of words from main dictionary
+    //track players [format: Map <PlayerID <letter_type, num_letters_of_type>]
+    Map<Integer, Map<Character, Integer>> playerCounts;
+
+    //track letters
+    int[] absoluteCounts;
+    int[] remainingCounts;
+    int[] values;
+    int[] probabilities;
+
+    //track my progress
+    String currentBest;
+    String currentTarget;
+    int cashRemaining;
+
+    //track total played (i.e. successfully auctioned) letters (currently does not include secret letters)
+    int totalAuctioned;
+
+    //dictionaries
+    String[] sortWords;                                            //sorted list of words from main dictionary
     String[] highValueWords;
 
 
-    public Group3Player() throws FileNotFoundException {
+    public Group3Player() {
 
-        //to enable tracking
         playerCounts = new HashMap<>();
-        absoluteCounts = new HashMap<>();
-        totalPlayed = 0;
+        absoluteCounts = new int[26];
+
+        totalAuctioned = 0;
+        cashRemaining = 100;
+
+        initialiseRemaining();
+        initialiseValues();
+
+        currentBest = "";
+        currentTarget = "aeimnorsstty";
 
     }
 
+
     //for use in initialisation
     protected void initializeSort() {
-        String line = null;
+        String line;
         ArrayList<String> wtmp = new ArrayList<>(55000);
         try {
             BufferedReader r = new BufferedReader(new FileReader("files/cleaned.txt"));
@@ -42,29 +66,60 @@ public class Group3Player extends Player {
         }
         sortWords = wtmp.toArray(new String[0]);
     }
+    private void initialiseRemaining() {
+        remainingCounts = new int[26];
+        for (int i = 0; i < 26; i++) remainingCounts[i] = getLetterFrequency((char) (toLetter(i)));
+    }
+    private void initialiseValues() {
+        values = new int[26];
+        for (int i = 0; i < 26; i++) {
+            Character character = toLetter(i);
+            values[i] = letterScore(character);
+        }
+    }
+
 
     //for use in player tracking
-    private void recordLetter(Letter letter, int ownerID) {
+    private void updateTrackers(Letter letter, int ownerID) {
 
         //check data
         if (letter == null) return;     //case letter is invalid
         if (ownerID < 0) return;       //case no one won this bid
 
         //if here, letter is valid
+        Character character = letter.getCharacter();
+        int index = toIndex(character);
 
-        // Add to the player count
-        if (!playerCounts.containsKey(ownerID)) playerCounts.put(ownerID, new HashMap<Letter, Integer>());      //case never seen this player win
-        else if (!playerCounts.get(ownerID).containsKey(letter)) playerCounts.get(ownerID).put(letter, 1);      //case this player has no letters of this type
-        else playerCounts.get(ownerID).put(letter, (playerCounts.get(ownerID).get(letter)+1));                  //case this player already has a letter of this type
+        // Add to player tracking
+        if (!playerCounts.containsKey(ownerID)) playerCounts.put(ownerID, new HashMap<>());      //case never seen this player win
+        else if (!playerCounts.get(ownerID).containsKey(character)) playerCounts.get(ownerID).put(character, 1);      //case this player has no letters of this type
+        else playerCounts.get(ownerID).put(character, (playerCounts.get(ownerID).get(character)+1));                  //case this player already has a letter of this type
 
-        //Add to the absolute count
-        if(!absoluteCounts.containsKey(letter)) absoluteCounts.put(letter, 1);
-        else absoluteCounts.put(letter, absoluteCounts.get(letter) + 1);
+        //Add to letter tracking
+        absoluteCounts[index] = absoluteCounts[index] + 1;
+        remainingCounts[index] = remainingCounts[index] - 1;
+        updateProbabilities();
+        totalAuctioned++;
 
-        //add to total seen
-        totalPlayed++;
+        //track banked progress
+        currentBest = returnWord();
 
     }
+    private void updateProbabilities() {
+
+        //set up vars
+        float[] probabilities = new float[26];
+        int totalRemaining = 26 - totalAuctioned;
+
+        //populate new array with probabilities
+        for (int i = 0; i < 26; i++) probabilities[i] = (float) remainingCounts[i] /totalRemaining;
+
+    }
+    private void updateCash(int pointsSpent) {
+
+        cashRemaining -= pointsSpent;
+    }
+
 
     //probable best move
     public float calculateExpectation(float probability, float value) {
@@ -76,6 +131,26 @@ public class Group3Player extends Player {
         for (int i = 0; i < probabilities.length; i++) temp += calculateExpectation(probabilities[i], values[i]);
         return temp;
     }
+    public int valueLetter(Character character) {
+
+        character = toLowerCase(character);
+
+        //case useful
+        for (int i = 0; i < currentTarget.length(); i++) if (currentTarget.charAt(i) == character) {
+
+            switch (remainingCounts[toIndex(character)]) {
+                case 1: return 7;
+                case 2: return 6;
+                case 3: return 5;
+                default: return 4;
+            }
+        };
+
+        //case not useful
+        return 0;
+
+    }
+
 
     //next best move
     public static boolean containsAllLetters(String str, List<Character> letters) {
@@ -103,13 +178,14 @@ public class Group3Player extends Player {
 
     }
 
+
     //generic helper functions
     public static List<Character> sorter( List<Character> letters){
         List<Character> sortedList = new ArrayList<>(letters);
         Collections.sort(sortedList);
         return sortedList;
     }
-    public static int toNumber(Character letter) {
+    public static int toIndex(Character letter) {
 
         letter = String.valueOf(letter).toLowerCase().charAt(0);
         return letter - 'a';
@@ -118,7 +194,11 @@ public class Group3Player extends Player {
     public static Character toLetter(int number) {
         return (char) (number + 'a');
     }
-    
+    public static Character toLowerCase(Character character) {
+        return String.valueOf(character).toLowerCase().charAt(0);
+    }
+
+
     //override functions
     @Override
     public void startNewGame(int playerID, int numPlayers) {
@@ -129,14 +209,14 @@ public class Group3Player extends Player {
     }
     @Override
     public void startNewRound(SecretState secretstate){
-        totalPlayed = 0;
+        totalAuctioned = 0;
         myLetters.clear(); // clear the letters that I have
         // this puts the secret letters into the currentLetters List
         myLetters.addAll(secretstate.getSecretLetters().stream().map(Letter::getCharacter).toList());
 
         playerLetters.clear(); // clear the letters that all the players have
         for (int i = 0; i < numPlayers; i++) {
-            playerLetters.add(new LinkedList<Character>()); // initialize each player's list of letters
+            playerLetters.add(new LinkedList<>()); // initialize each player's list of letters
         }
         /*
         Note that although the letters that I have will be in the playerLetters List, the playerLetters
@@ -144,30 +224,27 @@ public class Group3Player extends Player {
          */
     }
 
+
     //main function
     @Override
     public int bid(Letter bidLetter, List<PlayerBids> playerBidList, int totalRounds, ArrayList<String> playerList, SecretState secretstate, int playerID) {
 
-        //bank any new info about bids
-        if (playerBidList.size() > 0) recordLetter(playerBidList.get(playerBidList.size()-1).getTargetLetter(), playerBidList.get(playerBidList.size()-1).getWinnerID());
+        int proposal = 0;
 
-        //set up vars
-        String currBest = returnWord();
-        int roundsLeft = numPlayers * 8 - totalPlayed;
+        //update tracking [LETTER COUNTS, PROBABILITIES, BEST WORDS AVAILABLE]
+        if (!playerBidList.isEmpty()) updateTrackers(playerBidList.get(playerBidList.size()-1).getTargetLetter(), playerBidList.get(playerBidList.size()-1).getWinnerID());
 
-        //case 1: first 3 rounds [FIXED LOW BID]
-        if(totalPlayed < 3) return 3;
+        //case 1: if we haven't yet achieved 100 [PRIORITISE FLEXIBILITY]
+        if (cashRemaining + getWordScore(currentBest) <= 101) proposal = valueLetter(bidLetter.getCharacter());
 
-        //case 2: longer than 7 [ONLY BID ON AN IMMEDIATE WIN]
-        if (currBest.length() >= 7) if (immediateWinGuaranteed(bidLetter)) return 5;
+        //case 2: longer than 7 [PRIORITISE IMMEDIATE WIN]
+        else if (immediateWinGuaranteed(bidLetter)) proposal = 5;
 
-        //case 3: words shorter than 7
+        //update tracking [CASHFLOW]
+       updateCash(proposal);
 
-        //for each word that is potentially available...
-        //calculate the expectation of making that word
-        //apply a premium for words that are either highly flexible or high scoring
-
-        return 0;
+       System.out.println(proposal);
+       return proposal;
 
     }
 
